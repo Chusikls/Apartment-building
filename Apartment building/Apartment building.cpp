b572af8707c8bc1dbdeef28449e147f20def1b5a
Chusikls <Chusikl@yandex.ru>
2024-12-09 08:56:33 +0300

    Добавьте файлы проекта.

---
+++ Apartment building/Apartment building.cpp
@@ -0,0 +1,24 @@
+﻿#include <iostream>
+
+int main()
+{
+    std::string residents[10];
+    for (int n = 0; n < 10; n++) {
+        std::cout << "Enter last name: \n";
+        std::cin >> residents[n];
+    }
+    for (int n = 0; n < 3; n++) {
+        int i = 0;
+        std::cout << "Enter apartment number: \n";
+        std::cin >> i;
+        i--;
+        if (i <= 10 && i > -1) {
+            std::cout << residents[i] << '\n';
+        }
+        else std::cout << "No apartment";
+
+    }
+    
+}
+
+
